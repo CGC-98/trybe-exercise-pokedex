aabfd45259abb6468960d3ff9882d6f4059d71c7
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-10-21 21:14:51 -0300

    pokedex completion lint

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Pokemon extends React.Component {
   render() {
@@ -24,14 +24,16 @@ class Pokemon extends React.Component {
 }
 
 Pokemon.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  pokemon: propTypes.object.isRequired,
-  // id: propTypes.string.isRequired,
-  // name: propTypes.string.isRequired,
-  // type: propTypes.string.isRequired,
-  // value: propTypes.string.isRequired,
-  // measurementUnit: propTypes.string.isRequired,
-  // image: propTypes.string.isRequired,
-};
+  pokemon: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.string,
+      measurementUnit: PropTypes.string,
+    }),
+    image: PropTypes.string,
+  }),
+}.isRequired;
 
 export default Pokemon;
